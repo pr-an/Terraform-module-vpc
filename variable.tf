d50bcb754df73360dd09101e32bd3ab27beabe5f
pr-an <107716211+pr-an@users.noreply.github.com>
2026-04-19 19:59:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,15 +1,56 @@
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev/stage/prod)"
   type        = string
 }
 
-variable "vpc_name" {
-  description = "Name tag for the VPC"
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "bucket_suffix" {
+  description = "Suffix for S3 bucket name"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
   type        = string
 }
 
 variable "tags" {
-  description = "Map of tags to apply to all resources"
+  description = "Common tags"
   type        = map(string)
   default     = {}
+}
+
+# Subnets input (VERY IMPORTANT for your current design)
+variable "subnets" {
+  description = "Subnet configuration"
+  type = map(object({
+    cidr       = string
+    az         = string
+    is_public  = bool
+  }))
+}
+
+# EC2 instances input (VERY IMPORTANT for for_each)
+variable "ec2_instances" {
+  description = "EC2 instance config"
+  type = map(object({
+    ami_id        = string
+    instance_type = string
+    subnet_key    = string
+  }))
 }
