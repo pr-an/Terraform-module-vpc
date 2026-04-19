36e7b2ae3981539a1787111585c3266efb9f4c0b
pr-an <107716211+pr-an@users.noreply.github.com>
2026-04-19 19:58:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,27 @@ resource "aws_vpc" "this" {
     Name = var.vpc_name
   })
 }
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-igw"
+  })
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-public-rt"
+  })
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+}
